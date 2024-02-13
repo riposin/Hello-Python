4bf28aadec38481fadd189ab026125a2343a84d8
Ricardo Pool <ripostf@gmail.com>
2024-02-13 15:30:54 -0600

    Commit from WSL Ubuntu

---
+++ 00_hello.py
@@ -0,0 +1,21 @@
+# Hola mundo
+print("Hola Python")
+print('Hola Python')
+
+"""
+Comentario de multiples lineas
+1
+2
+3
+"""
+
+'''
+Comentario de multiples lineas
+1
+2
+3
+'''
+
+# Consultar el tipo de dato
+print(type("Hola")) # <class 'str'>
+print(type(2 + 3j)) # <class 'complex'>
