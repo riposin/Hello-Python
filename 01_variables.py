ae96882892a57125b90d8ab7393c5d9c4078aa70
Ricardo Pool <ripostf@gmail.com>
2024-02-13 17:59:27 -0600

    Second file

---
+++ 01_variables.py
@@ -0,0 +1,24 @@
+# Variables
+
+my_variable = 'My String Variable'
+print(my_variable)
+
+# Concatenate strings
+print("foo", "bar", my_variable)
+
+# NoneType
+print(type(print("some")))
+
+print(len(my_variable))
+
+# Define variables in a single line
+name, surname, alias, age = "John", "Doe", "Johnny", 35
+
+print('My name is:', name, surname, ', my nickname is:', alias, ' and I am',age, 'old.')
+
+# Inputs
+
+first_name = input('What is your name? ')
+age = input('How old are you? ')
+
+print('My name is:', first_name, 'and I am', age, 'years old.')
