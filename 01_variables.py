f9a534d97f655c1f3dd215729dcc0a62f44dd182
Ricardo Pool <ripostf@gmail.com>
2024-02-13 18:25:52 -0600

    Completed the file 01

--- 01_variables.py
+++ 01_variables.py
@@ -17,10 +17,15 @@ name, surname, alias, age = "John", "Doe", "Johnny", 35
 print('My name is:', name, surname, ', my nickname is:', alias, ' and I am',age, 'old.')
 
 # Inputs
-
+first_name = 'Rick'
+'''
 first_name = input('What is your name? ')
 age = input('How old are you? ')
+'''
 
 print('My name is:', first_name, 'and I am', age, 'years old.')
 
-# Conversion
+# Type definition, it does not work, it is only to help us to understand the intention of the code/variable
+address: str = 'Foo Bar'
+address = 123
+print(address)
