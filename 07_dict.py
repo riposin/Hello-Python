ed465c17252226d8da2a963b7b8ebf132db17826
Ricardo Pool <ripostf@gmail.com>
2024-02-16 14:06:34 -0600

    Completado, no mucho pero tiene

---
+++ 07_dict.py
@@ -0,0 +1,22 @@
+### Dictionaries ###
+
+c = 1
+
+my_dict = dict()
+my_dict = {}
+
+#1 
+my_other_dict = {
+	'name': 'Ricardo',
+	'age': 35,
+	'address': 'Calle Falsa 123'
+}
+my_dict = {
+	'name': 'Ricardo',
+	'age': 35,
+	'address': 'Calle Falsa 123',
+	'languages': {'Python', 'Java', 'C++'},
+	1:1.77
+}
+
+# It works like other languages, same methods to sort, order, add, remove, etc
