b858fe4ae25e4e654d4acccf941a23297c9a9e3f
Ricardo Pool <ripostf@gmail.com>
2024-02-16 15:47:10 -0600

    bloque completado 08

---
+++ 08_conditionals.py
@@ -0,0 +1,23 @@
+### Conditionals ###
+
+my_condition = True
+
+if my_condition:
+	print("IF 1")
+
+if 'a' == 'b':
+	print("IF 2")
+else:
+	print("ELSE 2")
+
+if 0 == 1:
+	print("IF 3")
+elif 5 == 5:
+	print("ELIF 3")
+else:
+	print("ELSE 3")
+
+if not (1 == 2):
+	print("IF 4")
+
+print("Execution continues")
